d762cbd9db981c9e24577ad0821f665525160ef8
Yun juyoung <yunju5090@gmail.com>
2024-09-30 06:16:43 +0900

    feat: tab bar

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -3,6 +3,7 @@ import 'package:flutter_food_delivery/components/myCurrentLocation.dart';
 import 'package:flutter_food_delivery/components/myDescriptionBox.dart';
 import 'package:flutter_food_delivery/components/myDrawer.dart';
 import 'package:flutter_food_delivery/components/mySliverAppBar.dart';
+import 'package:flutter_food_delivery/components/myTabBar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,35 +12,66 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  // tab controller
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MyDrawer(),
       body: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) => [
-                MySliverAppBar(
-                  title: Text("title"),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Divider(
-                        indent: 25,
-                        endIndent: 25,
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                      // my current location
-                      const MyCurrentLocation(),
-
-                      // description box
-                      const MyDescriptionBox(),
-                    ],
-                  ),
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+            title: MyTabBar(tabController: _tabController),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
                 ),
+                // my current location
+                const MyCurrentLocation(),
+
+                // description box
+                const MyDescriptionBox(),
               ],
-          body: Container(
-            color: Colors.blue,
-          )),
+            ),
+          ),
+        ],
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("first tab items"),
+            ),
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("second tab items"),
+            ),
+            ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Text("third tab items"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
