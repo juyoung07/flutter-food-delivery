a9a1fe4400a50450418720bb51fe4a37ff713028
juyoung07 <yunju5090@gmail.com>
2025-01-12 19:24:06 +1100

    흠.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery/auth/loginOrRegister.dart';
 import 'package:flutter_food_delivery/models/restaurant.dart';
 import 'package:flutter_food_delivery/themes/themeProvider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
